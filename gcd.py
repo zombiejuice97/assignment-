6b1bab3084eb33c8a4bb559b363b0b862ed62ca1
zombiejuice97 <azan16shaikh@gmail.com>
2022-02-16 18:06:47 +0530

    gcd single function

--- gcd.py
+++ gcd.py
@@ -1,20 +1,31 @@
-def factors(num):
+
+class gcd:
+    
+      
+    def __init__(self,num,num2):
+        self.num = number1
+        self.num2 = number2
     facs = []
-    for i in range(1,num+1):
+    facs2=[]
+    for i in range(1,number1+1):
         facs.append(i)
-    return facs
+    
+    for j in range(1,number2+1):
+        facs2.append(j)
 
-def Divisor(factors,y):
     divisors =[]
-    x = factors(y)
-    for i in x:
-        
-        if(y%i==0):
+    divisors2 =[]
+  
+
+    for k in facs:
+        if(number1%k==0):
             divisors.append(i)
-        
-    return divisors 
+    
+    for l in facs2:
+        if(number2%l==0):
+            divisors2.append(l)
+
+    print (divisors,divisors2)
 
 
-print(Divisor(factors,6))
-print(Divisor(factors,8))
-
+example2 = gcd(6,8)
